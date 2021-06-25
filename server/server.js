d2b3945cd7519d40d6ff8701a8d474d5d94469d0
Ubuntu <ubuntu@ip-172-31-27-246.us-west-2.compute.internal>
2021-06-25 23:57:37 +0000

    changes to server.js

---
+++ server/server.js
@@ -0,0 +1,33 @@
+let express = require('express');
+let mysql = require('mysql');
+
+const connection = mysql.createPool({
+  host     : 'localhost', // Your connection adress (localhost).
+  user     : 'jerry',     // Your database's username.
+  password : 'demo',        // Your database's password.
+  database : 'cointwits'   // Your database's name.
+});
+
+// Starting our app.
+const app = express();
+
+// Creating a GET route that returns data from the 'users' table.
+app.get('/user', function (req, res) {
+    // Connecting to the database.
+    connection.getConnection(function (err, connection) {
+
+    // Executing the MySQL query (select all data from the 'users' table).
+    connection.query('SELECT * FROM user', function (error, results, fields) {
+      // If some error occurs, we throw an error.
+      if (error) throw error;
+
+      // Getting the 'response' from the database and sending it to our route. This is were the data is.
+      res.send(results)
+    });
+  });
+});
+
+// Starting our server.
+app.listen(3000, () => {
+ console.log('Go to http://35.165.108.84:3000/user so you can see the data.');
+});
